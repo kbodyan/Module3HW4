801694b62182e5483d216182f58285552b9619e1
kbodyan <kbodyan@gmail.com>
2021-12-16 02:14:50 +0200

    task1_done

--- TwoTasks/TwoTasks/ClassA.cs
+++ TwoTasks/TwoTasks/ClassA.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
 
 namespace TwoTasks
 {
@@ -10,26 +11,27 @@ namespace TwoTasks
     {
         private double _result = 0;
         public event Func<double, double, double> SumEventHandler;
+        public event Action ActionEvent;
         public static double Sum(double x, double y) => x + y;
 
         public double Sum2(double x, double y) => _result += x + y;
 
         public void Run()
         {
-            SumEventHandler += Sum;
-            SumEventHandler += Sum;
-            Console.WriteLine($"First method: sum = {Executor(SumEventHandler, 2, 3)}");
+            var result = 0.0;
+            ActionEvent = () => result += Sum(2, 3);
+            ActionEvent += () => result += Sum(2, 3);
+            TryCatch(ActionEvent);
+            Console.WriteLine($"First method: sum = {result}");
             SumEventHandler = Sum2;
             SumEventHandler += Sum2;
             Console.WriteLine($"Second method: sum = {Executor(SumEventHandler, 2, 3)}");
-            {
-                double result = 0;
-                Func<double, double, double> func = (x, y) => result += x + y;
-                SumEventHandler = func;
-                SumEventHandler += func;
-
-                Console.WriteLine($"Third method: sum = {Executor(SumEventHandler, 2.0, 3.0)}");
-            }
+            result = 0;
+            Func<double, double, double> func = (x, y) => result += x + y;
+            SumEventHandler = func;
+            SumEventHandler += func;
+            Console.WriteLine($"Third method: sum = {Executor(SumEventHandler, 2.0, 3.0)}");
+            result = 0;
         }
 
         public object Executor(MulticastDelegate multicastDelegate, params object[] parameters)
@@ -38,26 +40,28 @@ namespace TwoTasks
             {
                 var list = multicastDelegate.GetInvocationList();
                 object result = null;
-                var sum = 0.0;
                 foreach (var item in list)
                 {
                     result = item.DynamicInvoke(parameters);
-                    if (result.GetType().IsValueType)
-                    {
-                        sum += (double)result;
-                    }
-                }
-
-                if (result.GetType().IsValueType)
-                {
-                    return sum;
                 }
 
                 return result;
             }
-            catch
+            catch (Exception e)
             {
-                return "Exeption";
+                return e;
+            }
+        }
+
+        public void TryCatch(Action code)
+        {
+            try
+            {
+                code();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
             }
         }
     }
